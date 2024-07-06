82fac7d8c98624d4b56f39c6d6b32551e1cbe705
Aashika100-jpg <aashika100pandey@gmail.com>
2024-07-06 20:45:28 +0545

    scientific calculator code.

---
+++ src/scientificcalculator/jTextfield1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package scientificcalculator;
+
+/**
+ *
+ * @author Aashley
+ */
+class jTextfield1 {
+
+    static void setText(String backSpace) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
